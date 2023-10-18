aa7f4334c4360044fe10f5db4e668f08c02c318d
JaksonMnz <jaksonmenezes96@gmail.com>
2023-10-17 23:40:08 -0300

    commit com adiçao das aulas de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,106 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && ( 7 > 4)){
+        System.out.println("as duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2) {
+            System.out.println("uma das condições é verdadeira");
+        }
+
+
+        System.out.println("fim");
+        // String nomeUm = "JAKSON";
+        // String nomeDois = new String("JAKSON");
+
+        // System.out.println(nomeUm.equals(nomeDois));
+
+        // int numero1 = 1;
+        // int numero2 = 2; 
+
+        // boolean simNao = numero1 == numero2;
+
+        // if (numero1 == numero2) {
+        // System.out.println("a nossa condição é verdadeira");
+        // }
+
+        // System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        // simNao = numero1 != numero2;
+
+        // System.out.println("numeroUm é diferente de numeroDois? " + simNao);
+
+        // simNao = numero1 > numero2;
+
+        // System.out.println("numeroUm é maior que numeroDois? " + simNao);
+
+
+
+        // int flamengo, botafogo;
+
+        // flamengo = 70;
+        // botafogo = 69;
+
+        // String resultado = flamengo==botafogo ?"BOTAFOGO CAMPEÃO" : "FLAMENGO CAMPEÃO";
+
+        // System.out.println(resultado);
+
+
+
+        // boolean variavel =true;
+        
+        // variavel = !variavel;
+
+        // System.out.println(variavel);
+
+
+        // int numero = 5;
+
+        // System.out.println(numero -- );
+
+        // System.out.println(numero);
+
+
+        // int numero = 5;
+
+        // numero = - numero;
+
+        // System.out.println(numero);
+        
+        // numero = numero * -1;
+
+        // System.out.println(numero);
+
+
+        // String concatenacao = "?";
+
+        // concatenacao = 1+1+1+"1";
+
+        // System.out.println(concatenacao);
+
+        // concatenacao = 1+"1"+1+1;
+
+        // System.out.println(concatenacao);
+
+        // concatenacao = 1+"1"+1+"1";
+
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1"+1+1+1;
+
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1"+(1+1+1);
+
+        // System.out.println(concatenacao);
+
+
+        
+    }
+    
+    
+}
--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,34 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        
+    
+        // TIPOS PRIMITIVOS 
+        //ESTUDEM A CLASSE STRING QUE REPRRESENTA TEXTO NA APLICAÇÃO
+
+
+        String meuNome = "JAKSON MENEZES";
+        System.out.println(meuNome);
+
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto; 
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+
+
+        int numero = 5;
+
+        numero = 10;
+
+    
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+      
     }
 }
