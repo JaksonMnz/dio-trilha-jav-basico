99142ee127869587f5a49e06305d374e1759d50a
JaksonMnz <jaksonmenezes96@gmail.com>
2023-10-19 18:59:04 -0300

    commit com adição da aula  tratamento de exceções

---
+++ tratamento-exceções/src/FormatadorCepExemplo.java
@@ -0,0 +1,19 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+        String cepFormatado = formatarCep("0357337");
+        System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O cep não contêm 8 digitos");
+            
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+        throw new CepInvalidoException();
+        
+        //simulando um cep formatador
+        return "23.765-064";
+    }
+}
+ 
