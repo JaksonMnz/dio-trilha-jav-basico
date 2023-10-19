546fb7d6003143e761f76cf64f60cb6fe4b33713
JaksonMnz <jaksonmenezes96@gmail.com>
2023-10-18 21:42:46 -0300

    adicionado a aula de estruturas condicionais java

---
+++ controle-fluxo/src/PlanpOperadora.java
@@ -0,0 +1,32 @@
+public class PlanpOperadora {
+    public static void main(String[] args) {
+
+        String plano = "T"; //B / M / T
+
+        switch (plano) {
+            case "T": {
+                System.out.println("5GB Youtube");
+            }
+            case "M": {
+                System.out.println("Whats e Instagram grátis");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
+
+        
+        // String plano = "T"; //B / M / T
+
+        // if(plano == "B") {
+        //     System.out.println("100 minutos de ligação");
+        // }else if(plano == "M") {
+        //     System.out.println("100 minutos de ligação");
+        //     System.out.println("Whats e Instagram grátis");
+        // }else if(plano == "T") {
+        //     System.out.println("100 minutos de ligação");
+        //     System.out.println("Whats e Instagram grátis");
+        //     System.out.println("5GB Youtube");
+        // }
+    }
+}
