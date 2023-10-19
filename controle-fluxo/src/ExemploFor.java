624f05455496aaf191a89c0dc986d0d8e9c86662
JaksonMnz <jaksonmenezes96@gmail.com>
2023-10-18 23:48:06 -0300

    commit: exemplos da aula de etrutura repetição for

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,17 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for ( int carneirinhos = 1; carneirinhos <=20 ; carneirinhos ++ ) {
+            System.out.println("contando carneirinhos " + carneirinhos);
+        }
+
+        System.out.println("Joãozinho dormiu");
+    }
+
+    // public static void main(String[] args) {
+    //     for ( int carneirinhos = 20; carneirinhos >=1; carneirinhos--) {
+    //         System.out.println("contando carneirinhos " + carneirinhos);
+    //     }
+
+    //     System.out.println("Joãozinho acordou");
+    // }
+}
